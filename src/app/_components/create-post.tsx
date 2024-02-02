167f7cf06e614c9b0745b6487ae604dd1b30af05
Mateo Delonga <74844680+delongamateo@users.noreply.github.com>
2024-02-02 17:24:50 +0100

    add ui

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { Button } from "~/components/ui/button";
 
 import { api } from "~/trpc/react";
 
@@ -22,7 +23,7 @@ export function CreatePost() {
         e.preventDefault();
         createPost.mutate({ name });
       }}
-      className="flex flex-col gap-2"
+      className="flex flex-col gap-8"
     >
       <input
         type="text"
@@ -31,13 +32,13 @@ export function CreatePost() {
         onChange={(e) => setName(e.target.value)}
         className="w-full rounded-full px-4 py-2 text-black"
       />
-      <button
+      <Button
+        variant={"secondary"}
         type="submit"
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
         disabled={createPost.isLoading}
       >
         {createPost.isLoading ? "Submitting..." : "Submit"}
-      </button>
+      </Button>
     </form>
   );
 }
